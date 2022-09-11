de6637968aaff1273fa270c35907ab0435676755
Marcus Oates <moates695@gmail.com>
2022-09-11 21:30:27 +1000

    still working through it

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// PREREQS /////////////////////////////////////////////////////////////////////
+
 const saved = localStorage.getItem("players");
 let players;
 if (saved !== null) {
@@ -20,10 +22,14 @@ function checkStorage() {
     }
 }
 
+// HELPERS /////////////////////////////////////////////////////////////////////
+
 function focusElement(element) {
     element.focus();
     element.select();
 }
+
+// GAME SETUP //////////////////////////////////////////////////////////////////
 
 function initGame() {
     document.getElementById("gameScreen").style.display = "block";
@@ -31,49 +37,22 @@ function initGame() {
     let scoreboard = document.getElementById("scoreboard");
     for (let name of Object.keys(players)) {
         let scoreCard = document.createElement("div");
-        scoreCard.setAttribute("id",`scoreCard:${name}`)
-        scoreCard.appendChild(document.createTextNode(`${name}: ${players[name]["score"]}`));
+        scoreCard.setAttribute("id",`scoreCard:${name}`);
+        scoreCard.setAttribute("class", "scoreCardActive");
+        
+        let nameNode = document.createElement("div");
+        nameNode.setAttribute("name", "name");
+        nameNode.appendChild(document.createTextNode(`${name}`));
+
+        let scoreNode = document.createElement("div");
+        scoreNode.setAttribute("name", "score");
+        scoreNode.appendChild(document.createTextNode(`${players[name]["score"]}`));
+
+        scoreCard.appendChild(nameNode);
+        scoreCard.appendChild(scoreNode);
+
         scoreboard.appendChild(scoreCard);
     }
-}
-
-function updatePlayerScore(score) {
-    for (let name of Object.keys(players)) {
-        if (players[name]["position"] != currPos) continue;
-        
-        if (players[name]["score"] + score == 50) {
-            return playerWin(name);
-        } else if (players[name]["score"] + score > 50) {
-            players[name]["score"] = 25;
-        } else {
-            if (score == 0) {
-                players[name]["consecutiveFouls"] += 1;
-                if (players[name]["consecutiveFouls"] == 3) forfeit(name);
-            } else {
-                players[name]["consecutiveFouls"] = 0;
-                players[name]["score"] += score;
-            }
-        }
-
-        cycleGameScreen(name);
-        break;
-    }
-}
-
-function cycleGameScreen(name) {
-    currPos += 1;
-    if (currPos >= Object.keys(players)) currPos = 0;
-
-    
-
-}
-
-function playerWin(name) {
-
-}
-
-function forfeit(name) {
-
 }
 
 document.getElementById("newGame").addEventListener("click", function(){
@@ -112,7 +91,7 @@ document.getElementById("addPlayer").addEventListener("click", function(){
 
     players[name] = {};
     players[name]["score"] = 0;
-    players[name]["position"] = Object.keys(players).length;
+    players[name]["position"] = Object.keys(players).length - 1;
     players[name]["consecutiveFouls"] = 0;
     players[name]["wins"] = 0;
     
@@ -143,6 +122,66 @@ document.getElementById("doneAddPlayer").addEventListener("click", function(){
     document.getElementById("addPlayers").style.display = "none";
     initGame();
 })
+
+
+// PLAY GAME ///////////////////////////////////////////////////////////////////
+
+function updatePlayerScore(score) {
+    for (let name of Object.keys(players)) {
+        if (players[name]["position"] != currPos) continue;
+
+        if (players[name]["score"] + score == 50) {
+            return playerWin(name);
+        } else if (players[name]["score"] + score > 50) {
+            players[name]["score"] = 25;
+        } else {
+            if (score == 0) {
+                players[name]["consecutiveFouls"] += 1;
+                if (players[name]["consecutiveFouls"] == 3) forfeit(name);
+            } else {
+                players[name]["consecutiveFouls"] = 0;
+                players[name]["score"] += score;
+            }
+        }
+
+        cycleGameScreen(name);
+        break;
+    }
+}
+
+function cycleGameScreen(name) {
+    currPos += 1;
+    if (currPos >= Object.keys(players).length) currPos = 0;
+
+    let scoreboard = document.getElementById("scoreboard");
+    let scoreCard;
+    for (let child of scoreboard.children) {
+        if (child.id != `scoreCard:${name}`) continue;
+        scoreCard = child;
+        scoreboard.removeChild(child);
+        break;
+    }
+
+    let inserted = false;
+    for (let child of scoreboard.children) {
+        if (child.id != /^scoreCard/) continue;
+        if (1) {
+
+        }
+    }
+    if (!inserted) {
+        scoreboard.appendChild(scoreCard);
+    }
+
+}
+
+function playerWin(name) {
+
+}
+
+function forfeit(name) {
+
+}
 
 document.getElementById("addScore").addEventListener("click", function(){
     let playerScore = document.getElementById("playerScore");
