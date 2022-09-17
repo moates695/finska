b16832b465d4b1d5d115b5024c525d1572888add
Marcus Oates <moates695@gmail.com>
2022-09-17 22:12:44 +1000

    most frontend base elements are made

--- ruleSet.js
+++ ruleSet.js
@@ -1,9 +1,10 @@
 export class RuleSet {
-    constructor(ruleSet="classic", pinValue="variable", missLimit=3, elimDuration=Infinity, winScore=50) {
+    constructor(ruleSet="classic", pinValue="variable", missLimit=3, elimDuration=Infinity, winScore=50, resetScore=25) {
         this.ruleSet = ruleSet;
         this.missLimit = missLimit;
         this.elimDuration = elimDuration;
         this.winScore = winScore;
+        this.resetScore = resetScore;
     }
 
     
