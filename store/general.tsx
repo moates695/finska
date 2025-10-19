94caef00405e984d3fa9e364cade9066f30643d7
Marcus Oates <marcus.oates@downergroup.com>
2025-10-19 15:08:59 +1100

    node modules

--- store/general.tsx
+++ store/general.tsx
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Theme, themes } from '@/styles/theme';
 
 const storage = createJSONStorage(() => AsyncStorage) as any;
-// AsyncStorage.clear(); //!!! 
+AsyncStorage.clear(); //!!! 
 
 export type Players = Record<string, string>;
 
@@ -135,7 +135,7 @@ export type CompleteState = 'win' | 'finish' | 'default';
 
 export const completeStateAtom = atom<CompleteState>('win');
 
-export const themeAtom = atomWithStorage<Theme>('themeAtom', themes.dark, storage, { getOnInit: true })
+export const themeAtom = atomWithStorage<Theme>('themeAtom', themes.sand, storage, { getOnInit: true })
 export const loadableThemeAtom = loadable(themeAtom);
 
 export const useDeviceThemeAtom = atomWithStorage<boolean>('useDeviceThemeAtom', false, storage, { getOnInit: true })
